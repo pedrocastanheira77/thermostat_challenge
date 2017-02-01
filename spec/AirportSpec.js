8e77d244e60769acd6aac4db6fa1bd6642057bb6
Pedro Castanheira <josepedrocastanheira@gmail.com>
2017-02-01 18:49:18 +0000

    Weather created Tests using spies and stubs

--- spec/AirportSpec.js
+++ spec/AirportSpec.js
@@ -1,50 +1,41 @@
 describe("Airport", function() {
-
   var airport;
   var plane;
   var weather
 
   beforeEach(function() {
     airport = new Airport()
-    plane = {};
+    plane = jasmine.createSpyObj("plane", ["changeStatus"]);
     plane2 = {};
     plane3 = {};
-    weather = {};
   });
 
-  describe("landing", function(){
-    it("lands a plane", function(){
-      plane.changeStatus = jasmine.createSpy("landed spy");
-      // weather.isStormy = jasmine.createSpy(false)
+  describe("when weather not stormy?", function(){
+    beforeEach(function() {
+      spyOn(airport.weatherReport, "isStormy").and.returnValue(false);
       airport.land(plane);
+    });
+
+    it("lands a plane", function(){
       expect(plane.changeStatus).toHaveBeenCalled();
-      expect(airport.showPlanes().length).toBe(1);
       expect(airport.showPlanes()).toContain(plane);
     });
 
-    it("won't land if stormy", function(){
-      plane.changeStatus = jasmine.createSpy("landed spy");
-      weather.isStormy = jasmine.createSpy("weather");
-      weather.isStormy.and.callFake(function() {
-        return true
-      });
-      expect(function() {airport.land(plane)}).toThrow();
-    });
-  });
-
-  describe("takeoff", function(){
     it("takeoff a plane", function(){
-      // weather.isStormy = jasmine.createSpy(false)
-      plane.changeStatus = jasmine.createSpy("flying spy");
       plane2.changeStatus = jasmine.createSpy("flying spy");
       plane3.changeStatus = jasmine.createSpy("flying spy");
-      airport.land(plane);
       airport.land(plane2);
       airport.land(plane3);
       airport.takeOff(plane2);
       expect(plane.changeStatus).toHaveBeenCalled();
-      expect(airport.showPlanes().length).toBe(2);
       expect(airport.showPlanes()).not.toContain(plane2);
+    });
+  });
+
+describe("when weather stormy?", function(){
+    it("won't land if stormy", function(){
+      spyOn(airport.weatherReport, "isStormy").and.returnValue(true);
+      expect(function() {airport.land(plane)}).toThrow("Unable to land, Weather is stormy");
     });
   });
 });
