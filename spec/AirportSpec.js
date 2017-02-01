7ca63b080fe265ae61fd67cb1358d5874a3aa796
Pedro Castanheira <josepedrocastanheira@gmail.com>
2017-02-01 17:03:06 +0000

    takeoff test not passing

--- spec/AirportSpec.js
+++ spec/AirportSpec.js
@@ -6,15 +6,25 @@ describe("Airport", function() {
   beforeEach(function() {
     airport = new Airport()
     plane = {};
-    plane.changeStatus = jasmine.createSpy("landed spy");
   });
 
   describe("landing", function(){
     it("lands a plane", function(){
+      plane.changeStatus = jasmine.createSpy("landed spy");
       airport.land(plane);
       expect(plane.changeStatus).toHaveBeenCalled();
       expect(airport.showPlanes().length).toBe(1);
       expect(airport.showPlanes()).toContain(plane);
     });
-  });  
+  });
+
+  describe("takeoff", function(){
+    it("takeoff a plane", function(){
+      plane.changeStatus = jasmine.createSpy("flying spy");
+      airport.takeoff(plane);
+      expect(plane.changeStatus).toHaveBeenCalled();
+      expect(airport.showPlanes().length).toBe(0);
+      expect(airport.showPlanes()).toContain(plane);
+    });
+  });
 });
