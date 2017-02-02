3a1c7b880c078abf791aacedbee561c1b6b49cf0
Pedro Castanheira <josepedrocastanheira@gmail.com>
2017-02-02 19:51:07 +0000

    Starting JQuery

---
+++ src/js/events.js
@@ -0,0 +1,12 @@
+$("#power_mode_btn").click(function(){
+    alert("The paragraph was clicked.");
+    
+});
+
+$("input#increase_btn").click(function(){
+    alert("The paragraph was clicked.");
+});
+
+$("input#decrease_btn").click(function(){
+    alert("The paragraph was clicked.");
+});
